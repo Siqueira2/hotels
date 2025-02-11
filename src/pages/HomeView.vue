460fb358303b088f5a53930854be7578024c1591
Siqueira <siqueiraguilherme920@gmail.com>
2025-02-10 23:31:02 -0300

    feat: Home view

---
+++ src/pages/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="row justify-center q-pa-md">
+    <div class="col-12 col-md-8 col-lg-6">
+      <PlaceSelector />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import PlaceSelector from 'src/components/PlaceSelector.vue'
+</script>
