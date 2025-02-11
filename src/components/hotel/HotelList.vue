af4b46b2840ea00167c418307fb8508baca6a4f0
Siqueira <siqueiraguilherme920@gmail.com>
2025-02-11 19:38:39 -0300

    refactor: improve loading feedback

--- src/components/hotel/HotelList.vue
+++ src/components/hotel/HotelList.vue
@@ -1,9 +1,19 @@
 <template>
   <q-infinite-scroll @load="onLoad">
-    <HotelCard v-for="hotel in hotels" :key="hotel.getId" :hotel="hotel as Hotel" />
+    <template v-if="hotels.length">
+      <HotelCard v-for="hotel in hotels" :key="hotel.getId" :hotel="hotel as Hotel" />
+    </template>
+
+    <div v-else-if="loading" class="row justify-center q-my-md">
+      <q-spinner-dots class="q-my-md" color="primary" size="40px" />
+    </div>
+
+    <div v-else class="row justify-center q-my-md">
+      <p class="text-grey-2">{{ $t('hotels.no-hotels') }}</p>
+    </div>
 
     <template #loading>
-      <div class="row justify-center q-my-md">
+      <div v-if="!loading && loadingNextPage" class="row justify-center q-my-md">
         <q-spinner-dots color="primary" size="40px" />
       </div>
     </template>
@@ -21,15 +31,22 @@ import HotelCard from 'src/components/hotel/HotelCard.vue'
 
 const hotelStore = useHotelStore()
 const { hotels } = storeToRefs(hotelStore)
+
 const page = ref(1)
+const loading = ref(true)
+const loadingNextPage = ref(false)
 
 const onLoad = async (_: number, done: VoidFunction) => {
+  loadingNextPage.value = true
   page.value++
   await hotelStore.fetchHotels(page.value)
+  loadingNextPage.value = false
   done()
 }
 
 onMounted(async () => {
+  loading.value = true
   await hotelStore.fetchHotels()
+  loading.value = false
 })
 </script>
