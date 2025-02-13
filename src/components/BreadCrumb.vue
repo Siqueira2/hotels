fd64e07af7646a68bac3a4fe4a0fbd24f81390c4
Siqueira <siqueiraguilherme920@gmail.com>
2025-02-13 18:52:37 -0300

    feat: dinamic breadcrumb

--- src/components/BreadCrumb.vue
+++ src/components/BreadCrumb.vue
@@ -5,6 +5,22 @@
     </template>
 
     <q-breadcrumbs-el :label="$t('shared.breadcrumbs.home')" to="/" />
-    <q-breadcrumbs-el :label="$t('shared.breadcrumbs.hotels')" to="/hotels" />
+    <q-breadcrumbs-el :label="$t('shared.breadcrumbs.hotels')" />
+    <template v-if="breadcrumbs">
+      <q-breadcrumbs-el :label="breadcrumbs" />
+    </template>
   </q-breadcrumbs>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+const route = useRoute()
+
+const breadcrumbs = computed(() =>
+  t('shared.breadcrumbs.accommodation', { suffix: route.query.name }),
+)
+</script>
